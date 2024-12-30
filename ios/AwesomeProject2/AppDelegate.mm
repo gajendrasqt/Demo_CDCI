517965a62c175d15f6b81744db28006664443793
Bhanderi-Aksahy <akshay.bhanderi@softqubes.com>
2024-12-30 19:48:12 +0530

    Revert "GS"
    This reverts commit a358ae27b0107851f33a112e1c746343b3606a30.

--- ios/AwesomeProject2/AppDelegate.mm
+++ ios/AwesomeProject2/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"CDCI_Project";
+  self.moduleName = @"AwesomeProject2";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/AwesomeProject2Tests/AwesomeProject2Tests.m
+++ ios/AwesomeProject2Tests/AwesomeProject2Tests.m
@@ -5,7 +5,7 @@
 #import <React/RCTRootView.h>
 
 #define TIMEOUT_SECONDS 600
-#define TEXT_TO_LOOK_FOR @"Welcome to CDCI PIPELINE .APK"
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
 
 @interface AwesomeProject2Tests : XCTestCase
 
